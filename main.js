bff06d5e00d6431ff8b94206fc720824abd894f4
wgetgoose <carson@carsonferreira.com>
2023-01-11 22:32:25 -0800

    Remove fluff & add notes

--- main.js
+++ main.js
@@ -4,14 +4,12 @@
 
     For future Docs, hello! If you need to email me for help, try carson@carsonferreira.com
 
-    Notes: "let" variables do not work with Photoshop, use "var". "const" works as normal.
+    Notes: 
+        - "let" variables do not work with Photoshop, use "var". "const" works as normal.
+        - Arrow functions are not supported
+        - File paths need to be forward slashed
 
  */
-
-
-const defaultUnit = preferences.rulerUnits
-preferences.rulerUnits = Units.INCHES
-
 
 // File paths needs to be forward-slash, not Windows backslash
 const averyTemplate = File("C:/Users/carson/coding/mun-credential-auto/Avery5390NameBadgesInsertRefills.psd")
@@ -21,14 +19,3 @@ var openTemplate = function() {
     app.open(averyTemplate)
 } 
 openTemplate()
-
-
-// var artLayerRef = docRef.artLayers.add()
-// artLayerRef.kind = LayerKind.TEXT
-
-// var textItemRef = artLayerRef.textItem
-// textItemRef.contents = "Hello, World"
-
-// docRef = null
-// artLayerRef = null
-// textItemRef = null

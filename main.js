2ba7c7fdfc96909cef13ecb9f7f249dc14b4e35d
wgetgoose <carson@carsonferreira.com>
2023-01-13 23:58:38 -0800

    Goodnight, see you in the morning!

--- main.js
+++ main.js
@@ -11,14 +11,40 @@
 
  */
 
-// File paths needs to be forward-slash, not Windows backslash
-const averyTemplate = File.openDialog(["Please select the template document"])
+// Asks the user to select the PSD and CSV
+// var averyTemplate = File.openDialog(["Please select the template document"],["Photoshop Document:*.psd"])
+var matrix = File.openDialog(["Please open the matrix CSV file"],["CSV:*.csv"])
+matrix.open("r")
+
+var lines = []
+
+
+// Makes programs hang like a motherfucker
+loadLines = function() {
+  matrix.readln()
+  // $.writeln(readLine)
+  $.writeln(matrix.eof)
+  for (var i = false; i != true;) {
+    if (matrix.eof == true) {
+      $.writeln("End of file")
+      i = true
+    }
+    else {
+      $.writeln("Moving!")
+      i = true // without this, it hangs photoshop and VSCode
+    }
+  }
+}
+
+loadLines()
 
 // Must set opening the document as a variable
-var docRef = app.open(averyTemplate)
+// var docRef = app.open(averyTemplate)
+// var test = matrix.readln()
 
-
-
+// var textLayer = docRef.artLayers.add()
+// textLayer.kind = LayerKind.TEXT
+// textLayer.textItem.contents = test
 
 
 /* 
@@ -30,8 +56,7 @@ Nevertheless, going to try and use CSV instead with Photoshop's file objects, se
 // var matrix = JSON.parse(File("C:/Users/carson/coding/mun-credential-auto/matrix.json"))
 // var firstObject = matrix[0]
 
-// var textLayer = docRef.artLayers.add()
-// textLayer.kind = LayerKind.TEXT 
+
 
 // var fill = textLayer.textItem
 // fill.contents = firstItem

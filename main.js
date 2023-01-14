78e9eb42e87fe410825161e638e75244132d54f4
wgetgoose <carson@carsonferreira.com>
2023-01-13 17:40:54 -0800

    Use file prompt dialog instead of hard path

--- main.js
+++ main.js
@@ -12,10 +12,26 @@
  */
 
 // File paths needs to be forward-slash, not Windows backslash
-const averyTemplate = File("C:/Users/carson/coding/mun-credential-auto/Avery5390NameBadgesInsertRefills.psd")
+const averyTemplate = File.openDialog(["Please select the template document"])
 
-// Arrow functions not supported
-var openTemplate = function() {
-    app.open(averyTemplate)
-} 
-openTemplate()
+// Must set opening the document as a variable
+var docRef = app.open(averyTemplate)
+
+
+
+
+
+/* 
+
+Loading the JSON seems to work, but performing actions on it is wonky. Maybe pasting it will be better?
+Nevertheless, going to try and use CSV instead with Photoshop's file objects, see https://extendscript.docsforadobe.dev/file-system-access/file-object.html
+
+*/
+// var matrix = JSON.parse(File("C:/Users/carson/coding/mun-credential-auto/matrix.json"))
+// var firstObject = matrix[0]
+
+// var textLayer = docRef.artLayers.add()
+// textLayer.kind = LayerKind.TEXT 
+
+// var fill = textLayer.textItem
+// fill.contents = firstItem

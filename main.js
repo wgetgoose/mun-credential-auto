0843ccf88a1ba9d08b31cfa2c259fbba8f5773bc
wgetgoose <carson@carsonferreira.com>
2023-01-16 16:32:17 -0800

    Writes to console without hangs until EOF

--- main.js
+++ main.js
@@ -18,47 +18,20 @@ matrix.open("r")
 
 var lines = []
 
-
-// Makes programs hang like a motherfucker
 loadLines = function() {
-  // $.writeln(readLine)
-  for (var i = 0; i < 5; i++) {
+  for (var i = true; i == true;) {
     var reading = matrix.readln()
     $.writeln(reading)
-    $.writeln(matrix.eof)
+    $.writeln("End of file: " + matrix.eof)
     if (matrix.eof == true) {
-      $.writeln("End of file")
-      i = 10
+      $.writeln("End of file!")
+      i = false
     }
     else {
-      $.writeln("Moving! Interation number " + i)
-      // i = true // without this, it hangs photoshop and VSCode
+      $.writeln("Moving! Iterating --> " + i)
     }
   }
   return
 }
 
 loadLines()
-
-// Must set opening the document as a variable
-// var docRef = app.open(averyTemplate)
-// var test = matrix.readln()
-
-// var textLayer = docRef.artLayers.add()
-// textLayer.kind = LayerKind.TEXT
-// textLayer.textItem.contents = test
-
-
-/* 
-
-Loading the JSON seems to work, but performing actions on it is wonky. Maybe pasting it will be better?
-Nevertheless, going to try and use CSV instead with Photoshop's file objects, see https://extendscript.docsforadobe.dev/file-system-access/file-object.html
-
-*/
-// var matrix = JSON.parse(File("C:/Users/carson/coding/mun-credential-auto/matrix.json"))
-// var firstObject = matrix[0]
-
-
-
-// var fill = textLayer.textItem
-// fill.contents = firstItem
